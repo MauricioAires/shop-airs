c9dcf8ad41ba76a668830c41536bf69db21da981
MauricioAires <mauricioaires015@gmail.com>
2023-03-03 00:19:36 -0300

    style(theme) config

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,19 +1,21 @@
-import { globalCss } from '.';
+import { globalCss } from '.'
 
 export const globalStyles = globalCss({
   '*': {
     margin: 0,
-    padding: 0
+    padding: 0,
   },
   body: {
-    '-webkit-font-smoothing': 'antialiased'
+    '-webkit-font-smoothing': 'antialiased',
+    backgroundColor: '$gray900',
+    color: '$gray100',
   },
   button: {
     border: 0,
-    cursor: 'pointer'
+    cursor: 'pointer',
   },
   'body, input, textarea,  button': {
     fontFamily: 'Roboto',
-    fontWeight: 400
-  }
-});
+    fontWeight: 400,
+  },
+})
