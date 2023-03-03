cbbb011f07cffc6d4e25158a8017335cda943c36
MauricioAires <mauricioaires015@gmail.com>
2023-03-03 00:30:29 -0300

    feat(_app) create header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,23 @@
 import { globalStyles } from '@/styles/global'
 import type { AppProps } from 'next/app'
 
+import logoImg from '@/assets/logo.svg'
+import Image from 'next/image'
+
 // NOTE: Load styles once
 globalStyles()
 
+import * as S from '@/styles/pages/app'
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <S.Container>
+      <S.Header>
+        <Image src={logoImg} alt="" />
+      </S.Header>
+      <Component {...pageProps} />
+    </S.Container>
+  )
 }
 /**
  * NOTE: File system Routing
