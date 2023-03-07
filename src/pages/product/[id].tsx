44cf1f5d753bd974ff20b471277fc2b90360d21d
MauricioAires <mauricioaires015@gmail.com>
2023-03-07 08:28:31 -0300

    feat(product) product page structure

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,6 +1,24 @@
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/router'
+
+import * as S from '../../styles/pages/product'
 
 export default function ProductPage() {
-  const { query } = useRouter();
-  return <h1>Product {JSON.stringify(query)} </h1>;
+  const { query } = useRouter()
+  return (
+    <S.ProductContainer>
+      <S.ImageContainer></S.ImageContainer>
+
+      <S.ProductDetails>
+        <h1>Camiseta Z</h1>
+        <span>R$ 79.90</span>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi
+          doloribus ipsa aliquid aspernatur saepe neque laboriosam minus facere,
+          ex reiciendis blanditiis maxime quos tenetur tempore accusamus modi
+          quaerat quae ducimus.
+        </p>
+        <button>Comprar Agora</button>
+      </S.ProductDetails>
+    </S.ProductContainer>
+  )
 }
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,6 +4,7 @@ export const globalStyles = globalCss({
   '*': {
     margin: 0,
     padding: 0,
+    boxSizing: 'border-box',
   },
   '::-webkit-scrollbar': {
     width: 9,
