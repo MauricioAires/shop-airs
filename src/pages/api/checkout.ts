92142a787124fd4e4bd76d19bc8e86a13270cb94
MauricioAires <mauricioaires015@gmail.com>
2023-03-09 00:06:07 -0300

    feat(api checkout) add CHECKOUT_SESSION_ID params
    retornar o id da compra na url

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -26,7 +26,7 @@ export default async function handle(
     })
   }
 
-  const successUrl = `${process.env.NEXT_URL}/success`
+  const successUrl = `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`
   const cancelUrl = `${process.env.NEXT_URL}/`
 
   const checkoutSession = await stripe.checkout.sessions.create({
