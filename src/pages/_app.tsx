c9dbb578521af136557fd72341478fb22b0ada78
MauricioAires <mauricioaires015@gmail.com>
2023-03-12 00:13:11 -0300

    feat(_app) config CartProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,27 +1,41 @@
-import { globalStyles } from '@/styles/global'
 import type { AppProps } from 'next/app'
 import Image from 'next/image'
+import Link from 'next/link'
+
+import { CartProvider } from 'use-shopping-cart'
 
 import logoImg from '@/assets/logo.svg'
+
+import { ShoppingCart } from '@/components/ShoppingCart'
+
+import { globalStyles } from '@/styles/global'
 
 // NOTE: Load styles once
 globalStyles()
 
 import * as S from '@/styles/pages/app'
-import Link from 'next/link'
-import { ShoppingCart } from '@/components/ShoppingCart'
+
+const STRIPE_PUBLIC_KEY = process.env.STRIPE_PUBLIC_KEY as string
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <S.Container>
-      <S.Header>
-        <Link href="/">
-          <Image src={logoImg} alt="" />
-        </Link>
-        <ShoppingCart />
-      </S.Header>
-      <Component {...pageProps} />
-    </S.Container>
+    <CartProvider
+      cartMode="checkout-session"
+      stripe={STRIPE_PUBLIC_KEY}
+      currency="BRL"
+      shouldPersist={true}
+      loading={<p aria-live="polite">Loading redux-persist...</p>}
+    >
+      <S.Container>
+        <S.Header>
+          <Link href="/">
+            <Image src={logoImg} alt="" />
+          </Link>
+          <ShoppingCart />
+        </S.Header>
+        <Component {...pageProps} />
+      </S.Container>
+    </CartProvider>
   )
 }
 /**
