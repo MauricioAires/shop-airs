3f3029ecf656c15d821dd4d73317775b269a877b
MauricioAires <mauricioaires015@gmail.com>
2023-03-14 19:35:04 -0300

    feat(shopping cart) base modal shopping cart

--- src/components/ShoppingCart/index.tsx
+++ src/components/ShoppingCart/index.tsx
@@ -1,13 +1,76 @@
-import { Handbag } from 'phosphor-react'
+import { Handbag, X } from 'phosphor-react'
+import { useShoppingCart } from 'use-shopping-cart'
+
+import * as Dialog from '@radix-ui/react-dialog'
 
 import * as S from './styles'
+import Image from 'next/image'
+
+type Product = {
+  id: string
+  name: string
+  image: string
+  formattedValue: string
+}
 
 export function ShoppingCart() {
-  return (
-    <S.ShoppingCartWrapper>
-      <Handbag weight="bold" size={24} />
+  const { removeItem, cartDetails, clearCart, formattedTotalPrice } =
+    useShoppingCart()
 
-      <span>1</span>
-    </S.ShoppingCartWrapper>
+  let products: Product[] = []
+
+  if (cartDetails !== undefined) {
+    products = Object.values(cartDetails) as Product[]
+  }
+
+  return (
+    <>
+      <Dialog.Root>
+        <Dialog.Trigger asChild>
+          <S.ShoppingCartWrapper>
+            <Handbag weight="bold" size={24} />
+
+            {products.length && <span>{products.length}</span>}
+          </S.ShoppingCartWrapper>
+        </Dialog.Trigger>
+
+        <Dialog.Portal>
+          <S.Overlay />
+          <S.Content>
+            <S.MainContent>
+              <S.Title>Sacola de compras</S.Title>
+              <S.Close>
+                <X size={24} weight="bold" />
+              </S.Close>
+
+              <S.ListProducts>
+                {products.map((product) => (
+                  <li key={product.id}>
+                    <Image src={product.image} width={100} height={93} alt="" />
+                    <div>
+                      <p>{product.name} </p>
+                      <strong>{product.formattedValue}</strong>
+                      <button type="button">Remover</button>
+                    </div>
+                  </li>
+                ))}
+              </S.ListProducts>
+            </S.MainContent>
+
+            <footer>
+              <div>
+                <span>Quantidade</span>
+                <span>3 items</span>
+              </div>
+              <div>
+                <strong>Valor total</strong>
+                <strong>$270,00</strong>
+              </div>
+              <button>Finalizar compra</button>
+            </footer>
+          </S.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
+    </>
   )
 }
--- src/components/ShoppingCart/styles.ts
+++ src/components/ShoppingCart/styles.ts
@@ -1,5 +1,89 @@
 import { styled } from '@stitches/react'
 
+import * as Dialog from '@radix-ui/react-dialog'
+
+export const Close = styled(Dialog.Close, {
+  background: 'transparent',
+  color: '$gray300',
+  position: 'absolute',
+  top: 24,
+  right: 24,
+  lineHeight: 0,
+})
+
+export const Title = styled(Dialog.Title, {
+  fontSize: '$lg',
+  color: '$gray100',
+})
+
+export const Content = styled(Dialog.Content, {
+  borderRadius: 6,
+  padding: '2.5rem 3rem',
+  background: '$gray800',
+  position: 'fixed',
+  height: '100vh',
+  top: 0,
+  right: 0,
+  width: '500px',
+  boxShadow: ' -4px 0px 30px rgba(0, 0, 0, 0.8)',
+  overflow: 'auto',
+
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-between',
+
+  footer: {
+    display: 'flex',
+    flexDirection: 'column',
+    marginTop: '2rem',
+
+    button: {
+      backgroundColor: '$green500',
+      color: '$white',
+      borderRadius: 8,
+      padding: '1.25rem',
+      fontWeight: 'bold',
+      fontSize: '$md',
+      marginTop: '2rem',
+
+      '&:disabled': {
+        opacity: 0.6,
+        cursor: 'not-allowed',
+      },
+
+      '&:not(:disabled):hover': {
+        backgroundColor: '$green300',
+        transition: 'background ease-in-out 0.2s',
+      },
+    },
+
+    span: {
+      fontSize: '1rem',
+      color: '$gray100',
+    },
+
+    strong: {
+      fontSize: '$md',
+      fontWeight: 'bold',
+      color: '$gray100',
+    },
+
+    div: {
+      display: 'flex',
+      justifyContent: 'space-between',
+      margin: '0.5rem 0',
+    },
+  },
+})
+
+export const Overlay = styled(Dialog.Overlay, {
+  position: 'fixed',
+  width: '100vw',
+  height: '100vh',
+  inset: 0,
+  background: 'transparent',
+})
+
 export const ShoppingCartWrapper = styled('button', {
   display: 'flex',
   justifyContent: 'center',
@@ -39,5 +123,71 @@ export const ShoppingCartWrapper = styled('button', {
     fontWeight: 'bold',
 
     border: 'solid 3px $gray900',
+  },
+})
+
+export const MainContent = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '2rem',
+})
+
+export const ListProducts = styled('ul', {
+  listStyle: 'none',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'flex-start',
+  alignItems: 'center',
+  gap: '1.5rem',
+
+  li: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    gap: '1.5rem',
+
+    width: '100%',
+
+    img: {
+      background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100%)',
+      borderRadius: 8,
+      objectFit: 'cover',
+    },
+
+    div: {
+      display: 'flex',
+      flexDirection: 'column',
+      gap: '0.25rem ',
+      alignItems: 'flex-start',
+
+      button: {
+        background: 'transparent',
+        color: '$green500',
+        fontSize: '$md',
+        fontWeight: 'bold',
+
+        display: 'flex',
+        justifyContext: 'center',
+        alignItems: 'center',
+
+        padding: '0.75rem 0 0.75rem 0',
+        lineHeight: 0,
+        borderRadius: 6,
+        '&:hover': {
+          color: '$green300',
+        },
+      },
+
+      p: {
+        fontSize: '$md',
+        color: '$gray300',
+      },
+
+      strong: {
+        fontSize: '$lg',
+        color: '$gray100',
+      },
+    },
   },
 })
